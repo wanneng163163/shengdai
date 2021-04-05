8c1dc8c27ca87a7660a334e7bb96330f398dda52
wanneng163163 <63229729+wanneng163163@users.noreply.github.com>
2021-04-05 18:05:10 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+//000000000000000000000000
+//off[1]='<a href="https://mp.weixin.qq.com/s?__biz=MzI5MDM2OTA4Mg==&mid=2247487713&idx=1&sn=256b679f9c1c6bff7d20f78d45739ca5&chksm=ec21abb4db5622a26163717650a0ed6282c366e60fa8c0f9bf4ada2a19ddc7b28875bcc6121f&token=463999182&lang=zh_CN&scene=21#wechat_redirect";>上海中心</a>';
+//x[2]=121.512999;y[2]=31.239585;
+
+var murl=[];var off=[];var x=[];var y=[];var z=[];//
+
+
+z[1]="121.47043,31.237279";
+off[1]="03/26, 上海, Celia702｜100｜#techno, #tech house";
+murl[1]="http://mp.weixin.qq.com/s?__biz=Mzk0NTE1NDcxMA==&mid=2247485908&idx=1&sn=6066acec4abdf8948828d1e2fc2ec122&chksm=c318f62df46f7f3b5e72bbe390b1829b4129c370432963babeacb9110d2adcd6c7df366fa5a6&scene=0&xtrack=1#rd";
+
+z[2]="121.437679,31.214815";
+off[2]='03/26, 上海, Celia by Pluse｜100｜#techno';
+murl[2]="http://mp.weixin.qq.com/s?__biz=MzU1ODE1NDY4MQ==&mid=2247493689&idx=2&sn=6a31d5a2844d1f3e5c5357b3c5ba4837&chksm=fc287eefcb5ff7f9392982e3cf702e284dcdee087191c4aea19e4365025150fa93947850e726&scene=0&xtrack=1#rd";
+
+z[3]="121.472848,31.229282";
+off[3]="03/26, 上海, 151 club｜100｜#house, #disco, #techno";
+murl[3]="http://mp.weixin.qq.com/s?__biz=MzI2NjM3MTkwNQ==&mid=2247486874&idx=4&sn=aa7a99afb1d4c166e3257fbac1f15dce&chksm=ea8e5bb4ddf9d2a212016519ff1e009d9e5fd52b1571a53327c730ac5a5fd147e56f16882bb6&scene=0&xtrack=1#rd";
